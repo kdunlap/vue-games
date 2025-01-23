b554154006c98e6915ea9923d6238bd2c53d2bea
Ken Dunlap <kdunlap4918@gmail.com>
2025-01-23 13:46:04 -0600

    feat: added consistent header to future game pages

--- pages/connect-four.vue
+++ pages/connect-four.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <p class="my-4"><nuxt-link to="/">&laquo; Back</nuxt-link></p>
-
-    <h1 class="text-3xl font-bold">Connect Four</h1>
-
+    <div class="flex justify-center items-center">
+      <p class="flex-1 text-left"><nuxt-link to="/">&laquo; Back</nuxt-link></p>
+      <h1 class="text-3xl font-bold text-center">Connect Four</h1>
+      <p class="flex-1">&nbsp;</p>
+    </div>
   </div>
 </template>
 
--- pages/rock-paper-scissors.vue
+++ pages/rock-paper-scissors.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <p class="my-4"><nuxt-link to="/">&laquo; Back</nuxt-link></p>
-
-    <h1 class="text-3xl font-bold">Rock, Paper, Scissors</h1>
-
+    <div class="flex justify-center items-center">
+      <p class="flex-1 text-left"><nuxt-link to="/">&laquo; Back</nuxt-link></p>
+      <h1 class="text-3xl font-bold text-center">Rock, Paper, Scissors</h1>
+      <p class="flex-1">&nbsp;</p>
+    </div>
   </div>
 </template>
 
